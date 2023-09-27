ff79034e810b319c3829b6b96f2ed098ec62c3ad
apollonorth <129547481+apollonorth@users.noreply.github.com>
2023-09-27 13:48:05 -0400

    Time: 44 ms (10.58%), Space: 45.2 MB (38.08%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -4,7 +4,7 @@ class Solution {
         for(int i = 0; i < nums.length; i++) {
             if(nums[i] == 0 && i < nums.length - 1) {
                 if(nums [i + 1] == 0) {
-                   for(int j = i + 2; j < nums.length; j++) {
+                     for(int j = i + 2; j < nums.length; j++) {
                         if(nums[j] != 0) {
                             nums[i] = nums[j];
                             nums[j] = 0;
