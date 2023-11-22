fe9f74ff7bc3b2385f55f1c044a0cb9dc0979bb1
apollonorth <129547481+apollonorth@users.noreply.github.com>
2023-11-22 11:46:22 -0500

    Time: 0 ms (100.00%), Space: 39.2 MB (44.23%) - LeetHub

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.java
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.java
@@ -6,10 +6,10 @@ class Solution {
         if(n == 1 || n == 2)
             return 1;
         
-        int a = 0, b = 0, c = 1;
+        int a = 0, b = 1, c = 1;
         int next = 0;
         
-        for(int i = 0; i < n - 1; i++) {
+        for(int i = 1; i < n - 1; i++) {
             next = a + b + c;
             a = b;
             b = c;
